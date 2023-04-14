a9ec0fadc717661db2c8e3009e90d7be93f3314a
Dylan Durand <dylan.durand88@outlook.fr>
2023-04-14 20:04:14 +0200

    Create signin view

--- frontend/src/Pages/SignIn/SignIn.jsx
+++ frontend/src/Pages/SignIn/SignIn.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import './SignIn.css';
 
+// Import method react-router-dom
+import {Link} from 'react-router-dom';
+
 export default function SignIn() {
   return (
-    <div>
-      Page de connexion
-    </div>
+      <main className="signin t--dark">
+        <section className="signin-container flex justify-content--center">
+          <div className='signin-form mx--auto'>
+            <div className="text--center">
+              <i className="fa fa-user-circle"></i>
+              <h1 className='title-md mt--md'>Sign In</h1>
+            </div>
+            <form className='mt--md'>
+              <div className="flex flex-column signin-form__field-input">
+                <label for="username" className='fw--700'>Username</label>
+                <input type="text" id="username" />
+              </div>
+              <div className="flex flex-column signin-form__field-input">
+                <label for="password" className='fw--700'>Password</label>
+                <input type="password" id="password" />
+              </div>
+              <div className="signin-form__field-remember mb--sm">
+                <input type="checkbox" id="remember-me" />
+                <label for="remember-me">Remember me</label>
+              </div>
+              <Link to={"/user"} className="signin-btn">
+                Sign In
+              </Link>
+              {/* <button type='submit' className="signin-btn">Sign In</button> */}
+            </form>
+          </div>
+        </section>
+      </main>
   )
 }
